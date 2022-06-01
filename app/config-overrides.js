a0433fa864ff671e233e39170826359c48d7690f
Aloy Das <gate.2017.aloy@gmail.com>
2022-06-01 12:12:51 +0530

    Port to typescript

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -1,35 +1,4 @@
-const { ProvidePlugin } = require('webpack');
-
-module.exports = function (config, env) {
-    return {
-        ...config,
-        module: {
-            ...config.module,
-            rules: [
-                ...config.module.rules,
-                {
-                    test: /\.(m?js|ts)$/,
-                    enforce: 'pre',
-                    use: ['source-map-loader'],
-                },
-            ],
-        },
-        plugins: [
-            ...config.plugins,
-            new ProvidePlugin({
-                process: 'process/browser',
-            }),
-        ],
-        resolve: {
-            ...config.resolve,
-            fallback: {
-                assert: require.resolve('assert'),
-                buffer: require.resolve('buffer'),
-                stream: require.resolve('stream-browserify'),
-                "path": false,
-                "fs": false
-            },
-        },
-        ignoreWarnings: [/Failed to parse source map/],
-    };
-};
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+    return config;
+}
